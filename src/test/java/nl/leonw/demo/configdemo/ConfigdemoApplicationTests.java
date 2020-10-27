faa7ecd8501c3b5635b3efe27a391b840d0adf6a
Leon <qaz@dds.nl>
2020-10-27 09:10:40 +0100

    Initial commir; from start.spring.io with lombok, actuator and spring security

---
+++ src/test/java/nl/leonw/demo/configdemo/ConfigdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.leonw.demo.configdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
